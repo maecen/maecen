ca16b0d8de953d2a022fad69365918e8dea213d8
Kristoffer Olesen <kristofferdo@gmail.com>
2016-05-14 14:23:21 +0200

    Provide `userAgent` for muiTheme

--- shared/container/App/App.js
+++ shared/container/App/App.js
@@ -3,7 +3,9 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import './App.scss'
 
-const muiTheme = getMuiTheme({ })
+const muiTheme = getMuiTheme({
+  userAgent: 'all'
+})
 
 function App (props) {
   return (
