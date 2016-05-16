465add69d0f8ea76eca1b0579e50acb3d3e6b33f
Andreas Larsen <andreas@larsenwork.com>
2016-05-16 09:55:10 +0200

    profile container style

--- shared/container/ProfileContainer.js
+++ shared/container/ProfileContainer.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { translate } from 'react-i18next'
 import ContentWrapper from '../components/ContentWrapper/ContentWrapper'
 import * as Actions from '../actions/actions'
+import s from './ProfileContainer.scss'
 
 import Form from '../components/Form/Form'
 import TextField from '../components/Form/TextField'
@@ -62,46 +63,54 @@ class ProfileContainer extends Component {
 
     return (
       <ContentWrapper>
-        <Form onSubmit={this.handleSubmit.bind(this)} model={user}
-          updateModel={this.updateModel.bind(this)}
-          errors={this.state.errors}>
+        <div className={s.card}>
+          <Form onSubmit={this.handleSubmit.bind(this)} model={user}
+            updateModel={this.updateModel.bind(this)}
+            errors={this.state.errors}>
 
-          <TextField
-            path={['name', 'first']}
-            label='First Name'
-            placeholder='Your first name'
-            disabled={!isEdit} />
-          <br />
+            <TextField
+              path={['name', 'first']}
+              label='First Name'
+              placeholder='Your first name'
+              disabled={!isEdit} />
+            <br />
 
-          <TextField
-            path={['name', 'last']}
-            label='Last Name'
-            placeholder='Your last name'
-            disabled={!isEdit} />
-          <br />
+            <TextField
+              path={['name', 'last']}
+              label='Last Name'
+              placeholder='Your last name'
+              disabled={!isEdit} />
+            <br />
 
-          <TextField
-            path='email'
-            label='Email'
-            placeholder='Your email address'
-            disabled={!isEdit} />
-          <br />
+            <TextField
+              path='email'
+              label='Email'
+              placeholder='Your email address'
+              disabled={!isEdit} />
+            <br />
 
-          { isEdit === false
-            ? <Button type='button'
-                onClick={this.toggleEdit.bind(this)}
-                label='Edit profile' />
-            : <div>
-                <Button
+            { isEdit === false
+              ? <span className={s.marginRight}>
+                  <Button type='button'
                   onClick={this.toggleEdit.bind(this)}
-                  label='Cancel' />
-                <Button label='Update' primary={true} />
-              </div>
-          }
+                  label='Edit profile' />
+                </span>
+              : <span>
+                  <span className={s.marginRight}>
+                    <Button
+                      onClick={this.toggleEdit.bind(this)}
+                      label='Cancel' />
+                  </span>
+                  <span className={s.marginRight}>
+                    <Button label='Update' primary={true} />
+                  </span>
+                </span>
+            }
 
-          <Button onClick={this.clearAuth.bind(this)}
-            label={t('logout')} />
-        </Form>
+            <Button onClick={this.clearAuth.bind(this)}
+              label={t('logout')} />
+          </Form>
+        </div>
       </ContentWrapper>
     )
   }
