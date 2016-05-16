64a91b19463f2658f68b0766f86374d7356cc976
Kristoffer Olesen <kristofferdo@gmail.com>
2016-05-16 09:17:48 +0200

    Add content wrapper for all content

--- shared/container/HomeContainer.js
+++ shared/container/HomeContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import HeaderContainer from './HeaderContainer'
-import FooterContainer from './FooterContainer'
+import ContentWrapper from '../components/ContentWrapper/ContentWrapper'
 import Icon from '../components/Graphics/Icon'
 import Button from '../components/Form/Button'
 import s from './HomeContainer.scss'
@@ -14,22 +13,18 @@ class HomeContainer extends Component {
     const { t } = this.props
 
     return (
-      <div className='wrapper'>
-        <HeaderContainer />
-        <div className='container'>
-          <div className={s.home}>
-            <Icon size='calc(12vh + 12vw)'
-              viewBox='0 0 832 997'
-              icon='maecen-detail'
-            />
-            <div className={s.tagline}>{t('tagline')}</div>
-            <Link to='/maecenates'>
-              <Button label={t('maecenate.seeAll')} />
-            </Link>
-          </div>
+      <ContentWrapper>
+        <div className={s.home}>
+          <Icon size='calc(12vh + 12vw)'
+            viewBox='0 0 832 997'
+            icon='maecen-detail'
+          />
+          <div className={s.tagline}>{t('tagline')}</div>
+          <Link to='/maecenates'>
+            <Button label={t('maecenate.seeAll')} />
+          </Link>
         </div>
-        <FooterContainer />
-      </div>
+      </ContentWrapper>
     )
   }
 }
