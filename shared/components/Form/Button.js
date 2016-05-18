5d7c24ce0a9de298d572075d99a55708372111d9
Kristoffer Olesen <kristofferdo@gmail.com>
2016-05-18 15:42:33 +0200

    Have the `Button` component take another parameter `flat` which allows us to more easily mix flat buttons into the design

--- shared/components/Form/Button.js
+++ shared/components/Form/Button.js
@@ -1,27 +1,26 @@
 import React, { PropTypes } from 'react'
 import Immutable from 'seamless-immutable'
 import MaterialRaisedButton from 'material-ui/RaisedButton'
+import MaterialFlatButton from 'material-ui/FlatButton'
 
 export default function Button (props, context) {
   props = Immutable(props)
+  const ButtonType = props.flat ? MaterialFlatButton : MaterialRaisedButton
 
   return (
-    <MaterialRaisedButton {...props} />
+    <ButtonType {...props.without('flat')} />
   )
 }
 
 Button.defaultProps = {
   type: 'button',
-  backgroundColor: '#C69C76',
-  hoverColor: '#cfab8b',
-  labelStyle: {
-    color: 'white'
-  }
+  flat: false
 }
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
   disabled: PropTypes.bool,
-  onClick: PropTypes.func
+  onClick: PropTypes.func,
+  flat: PropTypes.bool
 }
