b1b5919ef9ce3f2a1d00106af5e28ae23313f884
Kristoffer Olesen <kristofferdo@gmail.com>
2016-05-20 11:52:01 +0200

    Format the default error message to the client

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
 import UserRoutes from '../routes/user.routes'
 import MaecenateRoutes from '../routes/maecenate.routes'
+import { formatResponseError } from '../util/ctrlHelpers'
 
 const router = new Router()
 
 router.use(UserRoutes)
 router.use(MaecenateRoutes)
+
+router.use((err, req, res, next) => {
+  console.log(err.stack)
+  const errors = formatResponseError(err)
+  res.status(500).json({ errors })
+})
 
 export default router
