17aa1e0996515c8d4e751cee0c86fbe6e10800f9
Andreas Larsen <andreas@larsenwork.com>
2016-05-23 09:49:54 +0200

    web app compatible

--- server/server.js
+++ server/server.js
@@ -92,6 +92,7 @@ const renderFullPage = (html, initialState, i18n) => {
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="apple-mobile-web-app-capable" content="yes">
         <!-- TO-DO we need to translate this -->
         <title>Mæcen</title>
         <link rel="stylesheet" href=${cssPath} />
