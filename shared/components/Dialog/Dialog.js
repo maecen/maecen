1d9e1866b8752845160341023a39928b599930c6
Kristoffer Olesen <kristofferdo@gmail.com>
2016-05-23 18:21:08 +0200

    Have the dialog always be full hight and scrollable instead of the content’s of it being scrollable

--- shared/components/Dialog/Dialog.js
+++ shared/components/Dialog/Dialog.js
@@ -1,17 +1,11 @@
 import React, { PropTypes, Component } from 'react'
 import MaterialDialog from 'material-ui/Dialog'
 
-const customContentStyle = {
-  width: '90%',
-  maxWidth: '550px'
-}
-
 export default class Dialog extends Component {
   render () {
     return (
       <MaterialDialog
-        {...this.props}
-        contentStyle={customContentStyle}>
+        {...this.props}>
         {this.props.onClick
           ? <div onClick={this.props.onClick}>{this.props.children}</div>
           : this.props.children
@@ -26,6 +20,15 @@ Dialog.propTypes = {
 }
 
 Dialog.defaultProps = {
+  autoDetectWindowHeight: false,
+  style: {
+    overflowY: 'auto'
+  },
+  contentStyle: {
+    marginBottom: '32px',
+    width: '90%',
+    maxWidth: '550px'
+  },
   titleStyle: {
     paddingBottom: '0px'
   }
