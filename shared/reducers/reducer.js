fdfc83d6eaabc96781f72e328a6b1a55c2ccb567
Kristoffer Olesen <kristofferdo@gmail.com>
2016-05-24 13:02:32 +0200

    Clean up the reducer

--- shared/reducers/reducer.js
+++ shared/reducers/reducer.js
@@ -13,17 +13,23 @@ const app = (state = Immutable({
         'authUser': action.id,
         'requireAuthorization': false
       })
+
     case ActionTypes.CLEAR_AUTH_USER:
       return state.set('authUser', null)
+
     case ActionTypes.SET_MAECENATE:
       return state.set('maecenate', action.id)
+
     case ActionTypes.SET_MAECENATE_LIST:
       return state.set('maecenates', action.ids)
+
     case ActionTypes.REQUIRE_AUTHORIZATION:
       const val = action.url ? action.url : true
       return state.set('requireAuthorization', val)
+
     case ActionTypes.CANCEL_REQUIRE_AUTHORIZATION:
       return state.set('requireAuthorization', false)
+
     default:
       return state
   }
@@ -38,52 +44,6 @@ const entities = (state = Immutable({
 
   return state
 }
-
-/*
-
-const postReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ActionTypes.ADD_POST :
-      return {
-        posts: [{
-          name: action.name,
-          title: action.title,
-          content: action.content,
-          slug: action.slug,
-          cuid: action.cuid,
-          _id: action._id,
-        }, ...state.posts],
-        post: state.post };
-
-    case ActionTypes.CHANGE_SELECTED_POST :
-      return {
-        posts: state.posts,
-        post: action.slug,
-      };
-
-    case ActionTypes.ADD_POSTS :
-      return {
-        posts: action.posts,
-        post: state.post,
-      };
-
-    case ActionTypes.ADD_SELECTED_POST :
-      return {
-        post: action.post,
-        posts: state.posts,
-      };
-
-    case ActionTypes.DELETE_POST :
-      return {
-        posts: state.posts.filter((post) => post._id !== action.post._id),
-      };
-
-    default:
-      return state;
-  }
-};
-
-*/
 
 export { app }
 export { entities }
