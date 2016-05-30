351223cf1d0ade31af98c4b82dae7ce3df754b8e
Andreas Larsen <andreas@larsenwork.com>
2016-05-30 10:06:31 +0200

    Maecenate view styling

--- shared/components/Card/index.js
+++ shared/components/Card/index.js
@@ -1,10 +1,11 @@
 import Card from './Card'
 import CardContent from './CardContent.js'
 import CardTitle from './CardTitle.js'
-import { CardActions } from 'material-ui/Card'
+import { CardActions, CardHeader, CardText } from 'material-ui/Card'
 
 export { Card }
 export { CardContent }
 export { CardTitle }
 export { CardActions }
-
+export { CardHeader }
+export { CardText }
