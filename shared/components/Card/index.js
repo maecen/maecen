006fca78934ec921694b75393239a04167b14bec
Andreas Larsen <andreas@larsenwork.com>
2016-06-10 15:08:41 +0200

    [ADDED] carderror component

--- shared/components/Card/index.js
+++ shared/components/Card/index.js
@@ -3,6 +3,7 @@ import { CardActions, CardHeader } from 'material-ui/Card'
 export { default as default } from './Card'
 export { default as Card } from './Card'
 export { default as CardContent } from './CardContent'
+export { default as CardError } from './CardError'
 export { default as CardTitle } from './CardTitle'
 export { CardActions }
 export { CardHeader }
