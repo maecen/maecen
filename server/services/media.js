9abbfb9e5f59196800276c460aa53ff2b3619c92
Kristoffer Olesen <kristofferdo@gmail.com>
2016-07-06 11:42:14 +0200

    [ADD] Clearing all unused media, and claiming media, to streamline it more

--- server/services/media.js
+++ server/services/media.js
@@ -3,9 +3,28 @@ import { deleteFile } from '../util/fileUploader'
 
 // Database Calls
 // ==============
+export function claimMedia (mediaIds, objType, objId, trx) {
+  trx = trx || knex
+  return trx('media')
+    .where('id', 'in', mediaIds)
+    .andWhere('obj_id', null)
+    .update({ obj_id: objId, obj_type: objType })
+}
+
+export function deleteUnusedMedia (objType, objId, safeIds, trx) {
+  trx = trx || knex
+  return trx('media').whereNotIn('id', safeIds)
+    .andWhere('obj_id', objId)
+    .andWhere('obj_type', objType)
+    .select('id')
+    .then(media => media.map(o => o.id))
+    .then(deleteMediaIds => {
+      return deleteMedia(deleteMediaIds, trx)
+    })
+}
+
 export function deleteMedia (ids, trx) {
   trx = trx || knex
-  console.log('deleting this media', ids)
   return trx('media').where('id', 'in', ids).then(media => {
     const deleteFiles = Promise.all(media.map(media => deleteFile(media.url)))
     return trx('media').where('id', 'in', ids).del().then(() => deleteFiles)
