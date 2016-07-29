67707a50195d198eb94adba341b673f3f325cd1c
Andreas Larsen <andreas@larsenwork.com>
2016-07-29 11:58:06 +0200

    [ADDED] desktop favicons

--- server/index.js
+++ server/index.js
@@ -130,12 +130,16 @@ function renderTemplate (html, initialState, i18n) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="theme-color" content="#262626">
         <!-- TODO we need to translate this -->
         <title>Mæcen</title>
         <link rel="stylesheet" href=${cssPath} />
         <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,300italic,700'
           rel='stylesheet' type='text/css'>
         <link rel="apple-touch-icon" href="/app-icon.png">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#262626">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
       </head>
       <body>
         <div id="root">${html}</div>
