79c4405ef3af33fb7acbd073dbd5e4d416dc3959
Andreas Larsen <andreas@larsenwork.com>
2016-08-08 00:44:03 +0200

    [FIX] remove superfluous div

--- shared/container/MyPage/MyPageView.js
+++ shared/container/MyPage/MyPageView.js
@@ -6,16 +6,14 @@ import { Row, Col } from 'react-flexbox-grid/lib'
 
 export default function MyPageView () {
   return (
-    <div>
-      <Row>
-        <Col md={6} sm={12} xs={12}>
-          <YourMaecenatesContainer />
-          <YourMaecensContainer />
-        </Col>
-        <Col md={6} sm={12} xs={12}>
-          <YourProfileContainer />
-        </Col>
-      </Row>
-    </div>
+    <Row>
+      <Col md={6} sm={12} xs={12}>
+        <YourMaecenatesContainer />
+        <YourMaecensContainer />
+      </Col>
+      <Col md={6} sm={12} xs={12}>
+        <YourProfileContainer />
+      </Col>
+    </Row>
   )
 }
