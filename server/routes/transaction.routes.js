7144b033e93c4054d6a9cd097fa6d8a36134858c
Kristoffer Olesen <kristofferdo@gmail.com>
2016-08-17 12:07:02 +0200

    [UPDATE] Move the cron job, to a separate `/tasks` folder so it can be run as a separate process

--- server/routes/transaction.routes.js
+++ server/routes/transaction.routes.js
@@ -4,6 +4,5 @@ import * as transactions from '../controllers/transaction.controller'
 const router = new Router()
 
 router.get('/payment-callback', transactions.paymentCallback)
-router.get('/cron-refresh-subscriptions', transactions.cronRefreshSubscriptions)
 
 export default router
