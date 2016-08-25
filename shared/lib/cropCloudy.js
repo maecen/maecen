e842749c9d7c418f37e313f46e77977e99e50d31
Andreas Larsen <andreas@larsenwork.com>
2016-08-25 13:56:27 +0200

    [FIX] video bug (orientation, format and maxsize)

--- shared/lib/cropCloudy.js
+++ shared/lib/cropCloudy.js
@@ -4,9 +4,14 @@ export default function (url, type) {
   }
 
   let glue = ''
+  let path = ''
   const urlSplitted = url.split('upload')
 
   switch (type) {
+    case 'video':
+      glue = 'upload/w_720,h_720,f_mp4,c_fit'
+      path = urlSplitted[1]
+      break
     case 'cover':
       glue = 'upload/w_1200,h_675,c_fill'
       break
@@ -23,8 +28,10 @@ export default function (url, type) {
       glue = 'upload'
   }
 
-  // Change the file ending to jpg
-  const path = urlSplitted[1].replace(/\.\w{2,5}$/i, '.jpg')
+  // Change the file ending to jpg for images
+  if (type !== 'video') {
+    path = urlSplitted[1].replace(/\.\w{2,5}$/i, '.jpg')
+  }
 
   return urlSplitted[0] + glue + path
 }
